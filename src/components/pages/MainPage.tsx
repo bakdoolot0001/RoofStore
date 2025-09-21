29b1148cdff04c17ef51d0f436b864ccaa3335da
bakdoolot <nurisulajmanov.com>
2025-09-21 20:16:28 +0600

    fix mistake about

--- src/components/pages/MainPage.tsx
+++ src/components/pages/MainPage.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Welcome from "./main/welcome/Welcome";
+import About from "./main/about/About";
 
 const MainPage = () => {
   return (
     <section>
         <Welcome/>
+        <About/>
     </section>
   );
 };
