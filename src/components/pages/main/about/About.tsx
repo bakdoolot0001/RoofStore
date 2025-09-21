d6c908b8bcc0964563e23923467c8d96b072e076
bakdoolot <nurisulajmanov.com>
2025-09-21 20:43:34 +0600

    fix: add AboutCard

--- src/components/pages/main/about/About.tsx
+++ src/components/pages/main/about/About.tsx
@@ -1,10 +1,10 @@
-import AboutCard from "@/components/ui/about/AboutCard";
-import box from "@/assets/images/box.svg"
-import check from "@/assets/images/check.svg"
-import cart from "@/assets/images/cart.svg"
-import truck from "@/assets/images/truck.svg"
-import time from "@/assets/images/time.svg"
-import people from "@/assets/images/people.svg"
+import box from "../../../../assets/images/box.svg";
+import check from "../../../../assets/images/check.svg";
+import cart from "../../../../assets/images/cart.svg";
+import truck from "../../../../assets/images/truck.svg";
+import time from "../../../../assets/images/time.svg";
+import people from "../../../../assets/images/people.svg";
+import AboutCard from "../../../ui/about/AboutCard";
 import sass from "./About.module.scss";
 
 export interface AboutDataProps {
@@ -20,41 +20,41 @@ const aboutData: AboutDataProps[] = [
     title: "Постоянное наличие",
     description:
       "Прямая работа с производителем обеспечивает постоянное наличие всех видом металлопроката",
-      icon: check
+    icon: check,
   },
   {
     id: 2,
     title: "Собственный автопарк",
     description:
       "Автомобили грузоподъемностью от 1.5 до 20 тонн. Всегда быстрая доставка.",
-      icon: box
+    icon: box,
   },
   {
     id: 3,
     title: "Объемы поставок",
     description:
       "Собственная служба доставки, гаранатирует вам поставку материалов на объект в кратчайшие сроки.",
-      icon: truck
+    icon: truck,
   },
   {
     id: 4,
     title: "Справедливая цена",
     description: "Собственные ресурсы и транспорт позволяют снижать стоимость.",
-    icon: cart
+    icon: cart,
   },
   {
     id: 5,
     title: "Обработка заявки < 30 минут",
     description:
       "Отдел продаж, насчитывающий более 80 сотрудников, не оставит вашу заявку без внимания.",
-      icon: time
+    icon: time,
   },
   {
     id: 6,
     title: "Погрузка без очередей",
     description:
       "Развитая складская логистика позволяет отгружать продукцию всегда без очередей.",
-      icon: people
+    icon: people,
   },
 ];
 
