43eb7253c9708176a259a34863d22e517d91d6c5
bakdoolot <nurisulajmanov.com>
2025-09-22 17:06:11 +0600

    catalog

--- src/components/pages/MainPage.tsx
+++ src/components/pages/MainPage.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Welcome from "./main/welcome/Welcome";
 import About from "./main/about/About";
+import Catalog from "./main/katalog/Catalog";
 
 const MainPage = () => {
   return (
     <section>
         <Welcome/>
         <About/>
+        <Catalog/>
     </section>
   );
 };
